056e98e5ab3e4ed698a08779f4166d7cf9c38560
cassienoelle <cassienoelle@gmail.com>
2019-09-16 13:53:31 -0400

    push max test

---
+++ max_test/js/script.js
@@ -0,0 +1,27 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+let $count = 0;
+let $addNum = 1;
+let $control = 8;
+
+$(document).ready(function () {
+
+  metro();
+
+});
+
+function metro {
+  console.log($count);
+  // setInterval(()=> {
+  //   console.log(count);
+  //   count += addNum;
+  // }, 1000);
+}
